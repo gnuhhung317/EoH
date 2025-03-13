60935e285cd705868b291b20b489432b16639a8b
Duc Hung <duchung04st@gmail.com>
2025-03-14 00:43:12 +0700

    add logic for gemini model

--- eoh/src/eoh/llm/api_general.py
+++ eoh/src/eoh/llm/api_general.py
@@ -1,16 +1,25 @@
 import http.client
 import json
+from google import genai
 
 
 class InterfaceAPI:
-    def __init__(self, api_endpoint, api_key, model_LLM, debug_mode):
+    def __init__(self, api_endpoint, api_key, model_LLM : str, debug_mode):
         self.api_endpoint = api_endpoint
         self.api_key = api_key
         self.model_LLM = model_LLM
         self.debug_mode = debug_mode
         self.n_trial = 5
+        if(self.model_LLM.startswith('gemini')):
+            self.client = genai.Client(api_key=self.model_LLM)
 
     def get_response(self, prompt_content):
+        if(self.model_LLM.startswith('gemini')):
+                response = self.client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=prompt_content
+                )
+                return response.text
         payload_explanation = json.dumps(
             {
                 "model": self.model_LLM,
